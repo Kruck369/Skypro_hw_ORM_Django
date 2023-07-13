e7408356404c6109b05cd64f20089fa53350145f
Kruck <reka_931@mail.ru>
2023-07-13 20:58:45 +0300

    Согласно условий заданий 1-3.

---
+++ main/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение (превью)', **NULLABLE)
+    category = models.CharField(max_length=100, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    date_of_creation = models.DateTimeField(verbose_name='дата создания')
+    date_of_correction = models.DateTimeField(verbose_name='дата последнего изменения')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
