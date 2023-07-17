7d641e9c87c4d467fdaee311bbc0c83c2cbf8fa4
Kruck <reka_931@mail.ru>
2023-07-18 01:17:06 +0300

    С учетом выполнения ДЗ по теме "Шаблонизация"

--- main/views.py
+++ main/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
-# Create your views here.
+from main.models import Product
+
+
+def index(request):
+    """Главная страница с товарами"""
+    product_list = Product.objects.all()
+    items_per_page = 3
+    paginator = Paginator(product_list, items_per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'title': 'Главная'
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def contact(request):
+    """Страница с контактным данными"""
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f' {name} ({email}): {message}')
+
+    context = {
+        'title': 'Контакты'
+    }
+
+    return render(request, 'main/contact.html', context)
+
+
+def product(request, pk):
+    """Страница с детальным описанием продукта"""
+    product_detail = get_object_or_404(Product, pk=pk)
+
+    context = {
+        'product_detail': product_detail,
+        'desired_id': pk
+    }
+
+    return render(request, 'main/product.html', context)
